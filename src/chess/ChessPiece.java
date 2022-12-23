dba50c07daa6f300e267b451938a7cf16429737e
gstv <gstv.mra@gmail.com>
2022-12-23 20:19:18 -0300

    Implementing possible moves of king

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,19 +6,19 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
-    private Color color;
+	private Color color;
 
-    public ChessPiece(Board board, Color color) {
-        super(board);
-        this.color = color;
-    }
+	public ChessPiece(Board board, Color color) {
+		super(board);
+		this.color = color;
+	}
 
-    public Color getColor() {
-        return color;
-    }
-
-    protected boolean isThereOpponentPiece(Position position) {
-         ChessPiece p = (ChessPiece) getBoard().piece(position);
-         return p != null && p.getColor() != color;
-    }
+	public Color getColor() {
+		return color;
+	}
+	
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
